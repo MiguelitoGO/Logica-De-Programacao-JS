bcac772d62c5c0daaf6209c080f6b016dc0cd11c
Fabio Miguel <fmcontato99@gmail.com>
2024-06-14 22:52:29 -0300

    exercicios,fixando alguns conceitos

---
+++ exercicios/contador2.js
@@ -0,0 +1,7 @@
+//2. Crie um contador que começa em 10 e vá até 0 usando um loop while. Mostre cada número.
+let contador = 10;
+
+while (contador >= 0 ) {
+    alert(`contando, o loop rodou ${contador} vezes`)
+    contador--;
+}
---
+++ exercicios/contador3.js
@@ -0,0 +1,7 @@
+//3. Crie um programa de contagem regressiva. Peça um número e conte deste número até 0, usando um loop while no console do navegador.
+let numero = prompt("Digite um numero para a contagem regressiva")
+
+while (numero >= 0 ) {
+    console.log(numero)
+    numero--;
+}
