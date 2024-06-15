c603ec3db277068005a83232cd826c3be458a191
Fabio Miguel <fmcontato99@gmail.com>
2024-06-15 01:04:25 -0300

    Melhorar, refinando a logica utilizando operador ternario e randomizando numero secreto

--- app.js
+++ app.js
@@ -1,19 +1,16 @@
 alert('Boas vindas ao jogo do numero secreto.')
 
-let numeroSecreto = 5;
+let numeroSecreto = parseInt(Math.random() * 100 + 1);
 console.log(numeroSecreto);
 let chute;
 let tentativas = 1;
 
-
-
 //enquanto nao for igual n. secreto
 while (chute != numeroSecreto) {
-    chute = prompt('Escolha um numero entre 1 e 10');
-
+    chute = prompt('Escolha um numero entre 1 e 100');
     //se chute for igual ao numero secreto
     if (chute == numeroSecreto) {
-        alert(`Parabéns, você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+        break;
 
     } else {
         if (chute > numeroSecreto) {
@@ -24,3 +21,15 @@ while (chute != numeroSecreto) {
         tentativas++;
     }
 }
+
+//Operador ternario
+let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+alert(`Parabéns, você acertou o numero secreto ${numeroSecreto} com ${tentativas} ${palavraTentativa}.`);
+
+//Repare que o codigo abaixo foi substituido pelo operador ternario que resolve a mesmo questão para o sustantivo "tentativa" no singular e plural.
+/*if (tentativas > 1){
+    alert(`Parabéns, você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativas.`);
+} else {
+    alert(`Parabéns, você acertou o numero secreto ${numeroSecreto} com ${tentativas} tentativa.`);
+}
+*/
